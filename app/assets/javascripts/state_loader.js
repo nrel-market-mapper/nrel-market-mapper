22bc51b3faac50371e88652b7ee235543858158c
Julian Feliciano <julsfelic@gmail.com>
2016-04-20 17:22:24 -0600

    Add dynamic dropdown for states graphs
    Add a select dropdown that allows you to choose the state and dynamically
    update the graphs based on the states data

--- app/assets/javascripts/state_loader.js
+++ app/assets/javascripts/state_loader.js
@@ -1,20 +1,8 @@
-// $(document).ready(function() {
-//   $.getJSON("http://localhost:3000/api/v1/summaries?abbr=CO", function(data) {
-//     // Example of data we should be getting back
-//     var data = {
-//       costs: [5.23, 5.10, 5.01, 4.99, 4.87, 4.77, 4.67, 4.65, 4.59, 4.47, 4.39],
-//       installs: [5.23, 5.10, 5.01, 4.99, 4.87, 4.77, 4.67, 4.65, 4.59, 4.47, 4.39],
-//       sizes: [5.23, 5.10, 5.01, 4.99, 4.87, 4.77, 4.67, 4.65, 4.59, 4.47, 4.39]
-//     }
-//
-//     updateCharts(data);
-//   });
-// });
-
 var $stateSelect = $('#states');
-var state = $stateSelect.val();
 $stateSelect.on('change', function() {
-  $.post("http://localhost:3000/api/v1/summaries?abbr=" + state, function(data) {
+  var state = $stateSelect.val();
+
+  $.getJSON("http://localhost:3000/api/v1/summaries/find?state=" + state, function(data) {
     updateCharts(data);
   })
 });
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,4 +1,8 @@
 class StatesController < ApplicationController
   def index
   end
+
+  def show
+    @states = State.where.not(abbr: "US")
+  end
 end
