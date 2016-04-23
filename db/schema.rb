d120e5ea388b55e6a411ee949851a9ee64da3020
Julian Feliciano <julsfelic@gmail.com>
2016-04-23 13:44:51 -0600

    Add US map
    Added a new column to the states table to hold on to the geojson.
    Added a rake task to populate the us geojson column with its corresponding file
    Added necessary code to display the map

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419233439) do
+ActiveRecord::Schema.define(version: 20160423041617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160419233439) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "geojson"
   end
 
   create_table "summaries", force: :cascade do |t|
