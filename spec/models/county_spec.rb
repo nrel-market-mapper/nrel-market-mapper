dec57646a20cf593ab548aee7534f0f432ba2e2c
Kimiko Kano <kskano@gmail.com>
2016-04-25 14:38:05 -0600

    Add model test for county installs

--- spec/models/county_spec.rb
+++ spec/models/county_spec.rb
@@ -5,3 +5,11 @@ RSpec.describe County, type: :model do
   it { should have_many :zipcodes }
   it { should validate_presence_of :name }
 end
+
+RSpec.describe '#installs' do
+  it "returns number of installs for a county" do
+    county = create_county_with_10_installs
+
+    expect(county.installs).to eq 10
+  end
+end
